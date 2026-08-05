3e1fbbe87354f84b53eeeea693271509351b7583
Muhannadsa3d <mohand.sa3d2@gmail.com>
2026-08-05 18:10:50 +0300

    Add project files.

---
+++ G-NET-55-C#Advanced#01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace G_NET_55_C_Advanced_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
